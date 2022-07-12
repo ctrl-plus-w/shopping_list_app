46018c22405097791663f97de26757ac0cf87a1a
ctrl-plus-w <lukas.ldrn@gmail.com>
2022-07-12 14:18:08 +0200

    Build the Category class

--- lib/database/models/category/category.dart
+++ lib/database/models/category/category.dart
@@ -1,18 +1,65 @@
 import 'package:shopping_list_app/database/database.dart';
+import 'package:slugify/slugify.dart';
 import 'package:sqflite/sqflite.dart';
 
 const _tableName = "Category";
 
 class Category {
-  final int id;
-  final String slug;
-  final String name;
+  late int? id;
+  late String slug;
+  late String name;
 
   Category({
-    required this.id,
     required this.slug,
     required this.name,
+    this.id,
   });
+
+  Category.fromMap(Map<String, Object?> map) {
+    slug = map['slug'] as String;
+    name = map['name'] as String;
+    id = map['id'] as int;
+  }
+
+  @override
+  String toString() {
+    return 'Category(id: $id, name: $name, slug: $slug)';
+  }
+
+  static List<Category> toList(List<String> categories) {
+    return List.generate(
+      categories.length,
+      (index) => Category(
+        slug: slugify(categories[index]),
+        name: categories[index],
+      ),
+    );
+  }
+
+  Map<String, Object?> toMap() {
+    return {
+      "id": id,
+      "slug": slug,
+      "name": name,
+    };
+  }
+
+  static Future<List<Category>> getAll() async {
+    final database = await DatabaseHelper.database;
+
+    List<Map<String, Object?>> res = await database.query(
+      _tableName,
+      columns: ['id', 'slug', 'name'],
+    );
+
+    return res.map(Category.fromMap).toList();
+  }
+
+  Future<int> insert([Database? database]) async {
+    database = (database ?? await DatabaseHelper.database);
+
+    return await database.insert(_tableName, toMap());
+  }
 }
 
 Future<void> createCategoryTable(Database database) async {
